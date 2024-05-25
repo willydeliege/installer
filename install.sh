fdf0c172d446d264ada2c8f7cfeb4e8e5a81d13a
Frederic Willem <frederic.willem@gmail.com>
2024-05-25 18:08:46 +0200

    use chaotic-aur

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-sudo cp ./assets/pacman.conf ./assets/makepkg.conf /etc/ 
+sudo pacmam-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacmam-key --lsign-key 3056513887B78AEB
+sudo pacmam -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacmam -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+sudo cp ./assets/pacmam.conf ./assets/makepkg.conf /etc/ 
+
+sudo pacman -Syu
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
