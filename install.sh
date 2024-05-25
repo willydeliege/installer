f4cadf1a6a41515954810f77518b8036f24d1d71
Frederic Willem <frederic.willem@gmail.com>
2024-05-25 11:58:09 +0200

    update installation

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 sudo cp ./assets/pacman.conf ./assets/makepkg.conf /etc/ 
 
-yay --needed -S $(cat ./install_pac.pkg)
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ,,
+
+yay --needed --noconfirm -S $(cat ./install_pac.pkg)
 yay --needed --noconfirm -S $(cat ./install_yay.pkg)
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
