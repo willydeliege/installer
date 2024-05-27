7dfed0607a6e15a64e7e73748fd2de64d04a7ea8
Frederic Willem <frederic.willem@gmail.com>
2024-05-27 11:36:02 +0200

    update packages

--- install.sh
+++ install.sh
@@ -1,19 +1,5 @@
-#!/usr/bin/env bash
-# sudo pacmam-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-# sudo pacmam-key --lsign-key 3056513887B78AEB
-# sudo pacmam -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-# sudo pacmam -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-# sudo cp ./assets/pacmam.conf ./assets/makepkg.conf /etc/ 
-
-# sudo pacman -Syu
-
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ..
-
-yay --needed --noconfirm -S $(cat ./install_pac.pkg)
+sudo pacman -S --needed --noconfirm -S $(cat ./install_pac.pkg)
 yay --needed --noconfirm -S $(cat ./install_yay.pkg)
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- post-install.sh
+++ post-install.sh
@@ -32,6 +32,11 @@ sudo cp ./assets/bluetooth-disable-before-sleep.service /etc/systemd/system
 sudo systemctl enable bluetooth.service
 sudo systemctl enable bluetooth-disable-before-sleep.service
 
+# Disable autosuspend for btusb to make the bluetooth keyboard work again
+# this will erase any pre-existing contents from /etc/modprobe.d/btusb_disable_autosuspend.conf
+echo "options btusb enable_autosuspend=n" | sudo tee /etc/modprobe.d/btusb_disable_autosuspend.conf
+sudo dracut-rebuild
+
 
 # sudo cp ./assets/sddm.conf /etc/sddm.conf 
 # sudo systemctl enable sddm.service 
