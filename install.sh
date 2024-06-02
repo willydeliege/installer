eb529c12741c0036f894fab3a708f4729ab74e9a
Frederic Willem <frederic.willem@gmail.com>
2024-06-02 19:31:13 +0200

    prepare a better installation

--- install.sh
+++ install.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-sudo cp assets/pacman.conf /etc/pacman.conf
-sudo cp assets/makepkg.conf /etc/makepkg.conf
-sudo pacman -Syu
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ..
-yay -S --needed --noconfirm $(cat ./install_pac.pkg)
-yay --needed --noconfirm -S $(cat ./install_yay.pkg)
+
+git clone https://github.com/JaKooLit/Arch-Hyprland.git
+git clone https://gitlab.com/stephan-raabe/dotfiles.git
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
--- post-install.sh
+++ post-install.sh
@@ -27,24 +27,24 @@ cd /home/willefi/
 git clone https://github.com/willydeliege/wallpaper.git
 chezmoi init --apply willydeliege
 # chezmoi apply
-systemctl  --user enable swaync.service
+# systemctl  --user enable swaync.service
 
-sudo cp -r ./assets/interception/dual-function-keys ./assets/interception/udevmon.yaml /etc/interception
+# sudo cp -r ./assets/interception/dual-function-keys ./assets/interception/udevmon.yaml /etc/interception
 
-sudo systemctl enable udevmon.service
+# sudo systemctl enable udevmon.service
 
-localectl --no-convert set-x11-keymap us pc104 colemak_dh
-localectl --no-convert set-keymap mod-dh-ansi-us 
+# localectl --no-convert set-x11-keymap us pc104 colemak_dh
+# localectl --no-convert set-keymap mod-dh-ansi-us 
 
-sudo cp ./assets/bluetooth/main.conf /etc/bluetooth/main.conf
-sudo cp ./assets/bluetooth-disable-before-sleep.service /etc/systemd/system 
-sudo systemctl enable bluetooth.service
-sudo systemctl enable bluetooth-disable-before-sleep.service
+# sudo cp ./assets/bluetooth/main.conf /etc/bluetooth/main.conf
+# sudo cp ./assets/bluetooth-disable-before-sleep.service /etc/systemd/system 
+# sudo systemctl enable bluetooth.service
+# sudo systemctl enable bluetooth-disable-before-sleep.service
 
-sudo cp ./assets/sddm.conf /etc/sddm.conf 
-sudo systemctl enable sddm.service 
+# sudo cp ./assets/sddm.conf /etc/sddm.conf 
+# sudo systemctl enable sddm.service 
 
-notmuch new
+# notmuch new
 
-git config --global user.name "Frederic Willem"
+git config --global user.name "Frédéric Willem"
 git config --global user.email "frederic.willem@gmail.com"
