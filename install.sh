62c6cd244607342a363d3993c24ed32b0d216a42
Frédéric Willem <frederic.willem@gmail.com>
2024-06-06 07:35:24 +0200

    bluetooth problems

--- install.sh
+++ install.sh
@@ -2,7 +2,5 @@
 
 
 git clone https://github.com/JaKooLit/Arch-Hyprland.git
-git clone https://gitlab.com/stephan-raabe/dotfiles.git
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
