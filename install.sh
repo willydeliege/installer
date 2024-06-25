f225f9a88b235ee969d49c37f343bda33df4ae7b
Frédéric Willem <frederic.willem@gmail.com>
2024-06-25 06:51:25 +0200

    change jakoolit dir

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # paru -S --needed "$(cat install.pkg)"
-git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git ~/Arch-Hyprland
-cd ~/Arch-Hyprland || exit
+git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git ~/Repos/Arch-Hyprland
+cd ~/Repos/Arch-Hyprland || exit
 chmod +x install.sh
 ./install.sh
