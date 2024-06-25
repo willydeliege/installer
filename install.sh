33118c3f7b436dd035b9f54d9432cda52d884784
Frédéric Willem <frederic.willem@gmail.com>
2024-06-25 06:48:21 +0200

    doom sh installer

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 # paru -S --needed "$(cat install.pkg)"
 git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git ~/Arch-Hyprland
-cd ~/Arch-Hyprland
+cd ~/Arch-Hyprland || exit
 chmod +x install.sh
 ./install.sh
---
+++ install_doom.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
+~/.config/emacs/bin/doom install
