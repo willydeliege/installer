5ec798bc5cdd0c503300e05c3831637e1819ca8b
Frédéric Willem <frederic.willem@gmail.com>
2024-06-25 06:32:07 +0200

    jakoolit installer

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-paru -S --needed $(cat install.pkg)
-
-
+# paru -S --needed "$(cat install.pkg)"
+git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git ~/Arch-Hyprland
+cd ~/Arch-Hyprland
+chmod +x install.sh
+./install.sh
