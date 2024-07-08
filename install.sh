ce722b3d4a4c7d4729bac965cc9c5e19bcb2acf5
Frédéric Willem <frederic.willem@gmail.com>
2024-07-08 12:30:11 +0200

    update install scripts

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-
-# paru -S --needed "$(cat install.pkg)"
-git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git ~/Repos/Arch-Hyprland
-cd ~/Repos/Arch-Hyprland || exit
-chmod +x install.sh
-./install.sh
+sudo cp  ./assets/etc/* /etc
+sudo pacman -S --needed $(cat ./install_pac.pak)
+mkdir ~/Repos/
+ git clone https://aur.archlinux.org/paru.git ~/Repos/paru
+ (cd ~/Repos/paru || exit; makepkg -si)
+paru -S --needed $(cat ./install_paru.pak)
+git clone https://github.com/willydeliege/dotfiles.git ~/dotfiles
+(cd ~/dotfiles || exit; stow --adopt .)
+git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
+~/.config/emacs/bin/doom install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
