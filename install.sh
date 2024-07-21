6d005c95741fd31e8593896f083f4390acca5b4c
Frédéric Willem <frederic.willem@gmail.com>
2024-07-21 22:45:33 +0200

    use yay

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-sudo cp  ./assets/etc/* /etc
 sudo pacman -S --needed $(cat ./install_pac.pak)
-mkdir ~/Repos/
- git clone https://aur.archlinux.org/paru.git ~/Repos/paru
- (cd ~/Repos/paru || exit; makepkg -si)
-paru -S --needed $(cat ./install_paru.pak)
-git clone https://github.com/willydeliege/dotfiles.git ~/dotfiles
-(cd ~/dotfiles || exit; stow --adopt .)
+pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+yay -S --needed $(cat ./install_paru.pak)
+git clone --depth 1 https://github.com/willydeliege/dotfiles.git ~/dotfiles
+(cd ~/dotfiles || exit; stow *)
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 ~/.config/emacs/bin/doom install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
