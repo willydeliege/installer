f7883d9b2b2b567d6aca3ec8c7f50ac4ad62df21
Frédéric Willem <frederic.willem@gmail.com>
2024-07-23 08:23:59 +0200

    update packages scripts

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/env bash
-sudo pacman -S --needed $(cat ./install_pac.pak)
-pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-yay -S --needed $(cat ./install_paru.pak)
+#!/usr/bin/env sh
+sudo pacman -Syu
+sudo pacman -S --needed "$(cat ./install_pac.pak)"
+(git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si)
+yay -S --needed "$(cat ./install_aur.pak)"
 git clone --depth 1 https://github.com/willydeliege/dotfiles.git ~/dotfiles
-(cd ~/dotfiles || exit; stow *)
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-~/.config/emacs/bin/doom install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- update_packages.sh
+++ update_packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-pacman -Qqem > ./install_paru.pak
-sed '/yay/d' ./install_paru.pak > ./tmp
-mv ./tmp ./install_paru.pak
+pacman -Qqem > ./install_aur.pak
+sed '/yay/d' ./install_aur.pak > ./tmp
+mv ./tmp ./install_aur.pak
 pacman -Qqen > ./install_pac.pak
