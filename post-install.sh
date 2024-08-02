cf72166ea01badcd4d4985508c9f022bdb228209
Frédéric Willem <frederic.willem@gmail.com>
2024-08-02 23:44:26 +0200

    post-install script

--- post-install.sh
+++ post-install.sh
@@ -3,18 +3,16 @@
 
 sudo cp ./assets/interception/udevmon.yaml /etc/interception
 
-sudo systemctl enable udevmon.service
+sudo systemctl enable --now udevmon.service
 
 localectl --no-convert set-x11-keymap us pc104 colemak_dh
 localectl --no-convert set-keymap mod-dh-ansi-us
 
 # sudo cp ./assets/bluetooth/main.conf /etc/bluetooth/main.conf
-sudo systemctl enable bluetooth.service
 sudo cp ./assets/bluetooth-disable-before-sleep.service /etc/systemd/system
 sudo systemctl enable bluetooth-disable-before-sleep.service
 
-# sudo cp ./assets/sddm.conf /etc/sddm.conf 
-sudo systemctl enable sddm.service
+# sudo cp ./assets/sddm.conf /etc/sddm.conf
 
 # notmuch new
 
