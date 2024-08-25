8282f40e02eab87e40c5d3d022fa1a8da9ac1d31
Frédéric Willem <frederic.willem@gmail.com>
2024-08-25 09:53:31 +0200

    minimal archinstall

--- post-install.sh
+++ post-install.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
-
-
+sudo systemctl enable --now nordvpnd
 sudo cp ./assets/interception/udevmon.yaml /etc/interception
 
 sudo systemctl enable --now udevmon.service
 
-localectl --no-convert set-x11-keymap us pc104 colemak_dh
-localectl --no-convert set-keymap mod-dh-ansi-us
+sudo localectl --no-convert set-x11-keymap us pc104 colemak_dh
+sudo localectl --no-convert set-keymap mod-dh-ansi-us
 
 # sudo cp ./assets/bluetooth/main.conf /etc/bluetooth/main.conf
 sudo cp ./assets/bluetooth-disable-before-sleep.service /etc/systemd/system
 sudo systemctl enable bluetooth-disable-before-sleep.service
+sudo systemctl enable bluetooth
+sudo systemctl enable sddm
+sudo systemctl enable swaync
+sudo systemctl enable --now auto-cpufreq
 
 # sudo cp ./assets/sddm.conf /etc/sddm.conf
 
