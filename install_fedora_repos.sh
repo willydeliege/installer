4f2d442d0c83b22605b00816b78c81d7e02bcaed
Frédéric Willem <frederic.willem@gmail.com>
2024-09-16 04:45:24 +0200

    clone third parties

--- install_fedora_repos.sh
+++ install_fedora_repos.sh
@@ -20,11 +20,16 @@ sudo dnf install vivaldi-stable
 
 echo "Cloning repositories..."
 git clone https://github.com/willydeliege/dotfiles.git ~/.dotfiles/
-git clone https://github.com/willydeliege/wallpaper.git ~/wallpaper/
-
+git clone --depth 1 https://github.com/willydeliege/wallpaper.git ~/wallpaper/
+mkdir -p ~/Repos/
+git clone --depth 1 https://gitlab.com/interception/linux/tools.git ~/Repos/interception-tools/
+git clone --depth 1 https://gitlab.com/interception/linux/plugins/caps2esc.git ~/Repos/caps2esc/
+git clone --depth 1 https://gitlab.com/interception/linux/plugins/space2meta.git ~/Repos/space2meta/
+git clone https://github.com/gauteh/lieer.git ~/Repos/lieer/
+git clone --depth 1 https://github.com/nwg-piotr/nwg-drawer.git ~/Repos/nwg-drawer/
+git clone --depth 1 https://github.com/nwg-piotr/nwg-look.git ~/Repos/nwg-look/
 
 sudo dnf --refresh update
-sudo dnf install zsh eza fzf zoxide ripgrep fd-find gh shellcheck shfmt\
-    go rust enchant2 enchant2-devel pandoc stow \
-    emacs-gtk+x11 wlogout  \
+sudo dnf install zsh eza fzf zoxide ripgrep fd-find gh shellcheck shfmt go rust enchant2 enchant2-devel pandoc stow \
+    emacs-gtk+x11 wlogout \
     PackageKit-command-not-found
