bdea3c1c08fbb30dde7d2195b2212956e60966e2
Frédéric Willem <frederic.willem@gmail.com>
2024-10-01 10:52:16 +0200

    update fedora install script

--- install_fedora_repos.sh
+++ install_fedora_repos.sh
@@ -7,29 +7,21 @@ sudo dnf install -y --nogpgcheck https://download1.rpmfusion.org/free/fedora/rpm
 echo "Install nordvpn..."
 sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
 
-echo "Install swaync..."
-dnf copr enable erikreider/SwayNotificationCenter
-dnf install SwayNotificationCenter
-
-echo "Install terra repositories..."
-sudo dnf install --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' --setopt='terra.gpgkey=https://repos.fyralabs.com/terra$releasever/key.asc' terra-release
-
 echo "Install vivaldi..."
 sudo dnf config-manager --add-repo https://repo.vivaldi.com/archive/vivaldi-fedora.repo
 sudo dnf install vivaldi-stable
 
+# starship repo
+sudo dnf copr enable atim/starship
+
 echo "Cloning repositories..."
 git clone https://github.com/willydeliege/dotfiles.git ~/.dotfiles/
-git clone --depth 1 https://github.com/willydeliege/wallpaper.git ~/wallpaper/
 mkdir -p ~/Repos/
 git clone --depth 1 https://gitlab.com/interception/linux/tools.git ~/Repos/interception-tools/
 git clone --depth 1 https://gitlab.com/interception/linux/plugins/caps2esc.git ~/Repos/caps2esc/
 git clone --depth 1 https://gitlab.com/interception/linux/plugins/space2meta.git ~/Repos/space2meta/
 git clone https://github.com/gauteh/lieer.git ~/Repos/lieer/
-git clone --depth 1 https://github.com/nwg-piotr/nwg-drawer.git ~/Repos/nwg-drawer/
-git clone --depth 1 https://github.com/nwg-piotr/nwg-look.git ~/Repos/nwg-look/
 
 sudo dnf --refresh update
-sudo dnf install zsh eza fzf zoxide ripgrep fd-find gh shellcheck shfmt go rust enchant2 enchant2-devel pandoc stow \
-    emacs-gtk+x11 wlogout \
-    PackageKit-command-not-found
+sudo dnf install gh zsh fd-find tlp tlp-rdw git vim emacs pandoc shellcheck shfmt fzf bat eza starship enchant2-devel goobook cmake libevdev-devel systemd-devel yaml-cpp-devel boost-devel notmuch-devel notmuch nextcloud-client stow htop btop python3-devel python3-pip clang-tools-extra ripgrep npm hunspell-fr pass libtool automake autoconf pkg-config m4 wl-clipboard btop htop zoxide pam-devel
+sudo dnf remove power-profiles-daemon firefox
