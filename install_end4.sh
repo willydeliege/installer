07e00b75b43c95a4adbef02c7d5164613c455090
Frédéric Willem <frederic.willem@gmail.com>
2025-07-18 08:10:21 +0200

    add hyprland installers

---
+++ install_end4.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+cd ~/Downloads # whereever you want
+git clone https://github.com/end-4/dots-hyprland
+cd dots-hyprland
+./install.sh
---
+++ install_ml4w.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+# First insatll the hyprland WM
+
+flatpak install flathub com.ml4w.dotfilesinstaller
